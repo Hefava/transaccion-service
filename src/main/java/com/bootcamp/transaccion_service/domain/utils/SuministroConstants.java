4b56d4e3a64318d41e67bf5fb57df10b7eb5ca1f
hector vanegas <h.vanegas@utp.edu.co>
2024-09-24 13:41:24 -0500

    fecha de abastecimiento

--- src/main/java/com/bootcamp/transaccion_service/domain/utils/SuministroConstants.java
+++ src/main/java/com/bootcamp/transaccion_service/domain/utils/SuministroConstants.java
@@ -5,8 +5,14 @@ public class SuministroConstants {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
     }
 
+    public static final String FORMATO_FECHA = "yyyy-MM-dd";
+    public static final String ARTICULO_ABASTECIMIENTO = "El artículo se abastecerá el: ";
+    public static final Integer DIAS_ABASTECIMIENTO = 15;
+    public static final Integer MESES_ABASTECIMIENTO = 1;
+
     public static final String ERROR_EN_SOLICITUD = "Error en la solicitud: ";
     public static final String RECURSO_NO_ENCONTRADO = "Recurso no encontrado: ";
     public static final String ERROR_INTERNO_SERVIDOR = "Error interno del servidor";
     public static final String ERROR_DESCONOCIDO = "Error desconocido al leer la respuesta";
+    public static final String STOCK_SERRVER_ERROR = "El servicio de stock no está disponible, intente más tarde.";
 }
